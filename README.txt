79d468dcba2af9122493d80c5f3adfae09af727d
atakan <94399983+benevolentsocialplanner@users.noreply.github.com>
2022-06-24 18:14:29 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,11 @@
+Webscraping on Hepsiburada and GittiGidiyor.
+This code collects the data from the given inputs, exports them into json, csv 
+and excel then sends an alert email when the scraping is done.
+While giving inputs, make sure you put no space after the commas.
+You can change the proxy by entering the proxy that you want to use in the format of
+'1.1.1.1:1111'
+You will see on the code where to integrate your proxy
+Make sure you are using your own headers while compiling the code.
+Make sure pandas, BeautifulSoup is installed on your pc.
+After running the code, it will create the exported data format items in the directory
+that it is located on.
---
+++ hepsiburada.py
@@ -0,0 +1,95 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+import smtplib
+liste=[]
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36"}
+recipient = input("enter your mail addr")
+def proxy(proxy,url):    
+    
+    try:
+        r = requests.get(url,headers=headers,proxies={'http': proxy,'https':proxy},timeout=3)
+    except:
+        r = requests.get(url,headers=headers)
+        print("failed")
+    return r
+
+def proxy(url):
+    r = requests.get(url,headers=headers)
+    return r
+
+def hepsiburada():
+    url = input("enter the urls seperated by comma: ")
+    urlList = url.split(",")
+    for url in urlList:
+        
+        soup = BeautifulSoup(proxy(url).content,features="lxml") ###### change the proxy parameters if you want to change your proxy in line 27 and 33
+        try:
+            itemsfull = soup.find('ul',{"class":"productListContent-wrapper productListContent-grid-0"})
+            items = itemsfull.find_all('li',{"class":"productListContent-item"})
+            for item in items:
+                url="https://www.hepsiburada.com/"+(item.a.get("href")) 
+                soup = BeautifulSoup(proxy(url).content,features="lxml") ###### change the proxy parameters if you want to change your proxy in line 27 and 33
+                theprocess(url,soup)
+        except:
+            theprocess(url,soup)
+def theprocess(url,soup): 
+    dataDictionary={"website":"hepsiburada"}
+    price = soup.find('span',{"data-bind":"markupText:'currentPriceBeforePoint'"}).text
+    price_after_point = soup.find('span',{"data-bind":"markupText:'currentPriceAfterPoint'"}).text
+    price_currency = soup.find('span',{"itemprop":"priceCurrency"}).text
+    price = str(f"{price},{price_after_point} {price_currency}")
+    try:
+        oldPrice = soup.find('del',{'id':'originalPrice'}).text
+    except AttributeError:
+        oldPrice = None
+    imgMain = soup.find('img',{'class':'product-image'})['src']
+    title = soup.find('span',{'class':'product-name'}).text
+    try:
+        rating = soup.find('span',{'class':'rating-star'}).text.strip()
+    except AttributeError:
+        rating = None
+        ratingCount = None
+    count=0
+    listImages=[]
+    while True:
+        try:    
+            images = soup.find('img',{'id':f"image-{count}"})
+            count=count+1
+            images=images['data-src']
+            listImages.append(images)
+        except:
+            break
+    if rating != None:
+        ratingCount = soup.find('a',{'class':'product-comments'}).find('span').text
+    else:
+        ratingCount=None
+    dataDictionary.update({"price": f"{price}",'originalPrice':f'{oldPrice}','main img url': f'{imgMain}','title': f'{title}','rating': f'{rating}','image urls': f'{listImages}','rating count': f'{ratingCount}'})
+    liste.append([dataDictionary['website'],price,oldPrice,imgMain,title,rating,dataDictionary['image urls'],ratingCount])
+    df = pd.DataFrame(liste)
+    df.columns= ["website","price","old price","main img","title","rating","image urls","rating count"]
+    with pd.ExcelWriter('hepsiburada.xlsx', mode='w') as writer:
+        df.to_excel(writer, sheet_name='Sheet1')
+    df.to_csv('hepsiburada.csv')
+    with open('hepsiburada.json','w') as fp:
+        json.dump(liste, fp)
+    print("item added")
+        
+try:      
+    hepsiburada()
+except AttributeError:
+    print('no attributes found')
+
+email_provider = 'smtp-mail.outlook.com'
+sender = 'tousewithpyt@outlook.com'
+server = smtplib.SMTP(email_provider,port= 587)
+server.starttls()
+server.login(user = sender, password = 'python00')
+body = 'Webscraping completed'
+msg = f'From: {sender}\r\nTo: {recipient}\r\n\r\n{body}'
+
+server.sendmail(sender, recipient, msg)
+server.quit()
+
+
